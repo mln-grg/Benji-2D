3e219343b781e5ab8eb3531db5c072721134b6ab
mln-grg <mgeorge00747@gmail.com>
2022-03-24 18:04:20 +0530

    Testing the renderer

--- 2D_GameEngine/src/Game.cpp
+++ 2D_GameEngine/src/Game.cpp
@@ -1,25 +1,96 @@
 #include "Game.h"
+#include<iostream>
+#include <SDL.h>
+
 Game::Game() {
-	
+	isRunning = false;
 }
 Game::~Game() {
 	
 }
 void Game::Initialize() {
 
-}
-void Game::Run() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) 
+	{
+		std::cerr << "Error Initializing SDL"<<std::endl;
+		return;
+	}
+	else {
+		SDL_DisplayMode displayMode;
+		SDL_GetCurrentDisplayMode(0, &displayMode);
+		windowWidth = displayMode.w;
+		windowHeight = displayMode.h;
+			window = SDL_CreateWindow(
+			"Benji",
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			windowWidth,
+			windowHeight,
+			SDL_WINDOW_BORDERLESS
+		);
+		if (!window) {
+			std::cerr << "Error Creating SDL window" << std::endl;
+			return;
+		}
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+		if (!renderer) {
+			std::cerr << "Error creating SDL renderer." << std::endl;
+			return;
+		}
+		SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+		isRunning = true;
+	}
 
 }
 void Game::ProcessInput() {
+	SDL_Event sdlEvent;
+	while (SDL_PollEvent(&sdlEvent)) {
+		switch (sdlEvent.type) {
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+		case SDL_KEYDOWN:
+			if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+				isRunning = false;
+			}
+			break;
+		
+		}
+	}
+}
+void Game::Setup() {
 
 }
 void Game::Update() {
 
 }
 void Game::Render() {
+	SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+	SDL_RenderClear(renderer);
+	
+	//Drawing a test shape
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_Rect player = { windowWidth/2,windowHeight/2,100,100 };
+	SDL_RenderFillRect( renderer,&player );
+	SDL_RenderPresent(renderer);
+}
+
+
+void Game::Run() {
+	//Game Loop
+	Setup();
+	while (isRunning) 
+	{
+		ProcessInput();
+		Update();
+		Render();
+	}
 
 }
 void Game::Destroy() {
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
 }
--- 2D_GameEngine/src/Game.h
+++ 2D_GameEngine/src/Game.h
@@ -1,16 +1,25 @@
 #pragma once
+#include<SDL.h>
+
 class Game
 {
 private:
-
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	
 public:
 	Game();
 	~Game();
 	void Initialize();
 	void Run();
+	void Setup();
 	void ProcessInput();
 	void Update();
 	void Render();
 	void Destroy();
+
+	int windowWidth;
+	int windowHeight;
 };
 
